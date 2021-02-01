bea3311382c242e357cd59c405ddfd23a7579955
TimofeiShpak <60565887+TimofeiShpak@users.noreply.github.com>
2021-02-01 10:20:13 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
 module.exports = function reverse (n) {
-	let bool = 0;
-	if(n<0)bool=1;
-	if(bool)n=Math.abs(n);
-  let a = 0;
-  while(n>10){
-  	a+=(n%10);
-  	a*=10;
-  	n/=10;
-  	n= Math.trunc(n);
-  }
-  if(n==10)a=a*10+1;
-  if(n!=10)a+=n;
-  return a;
+  n = Math.abs(n);
+  return Number((''+n).split('').reverse().join(''));
 }
